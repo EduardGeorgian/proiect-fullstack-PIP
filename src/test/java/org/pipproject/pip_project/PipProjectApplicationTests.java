5a85c4e12d16f74c3ebe877cfd105d1a708e0634
eduard <edi.eduard1555@gmail.com>
2025-03-25 11:58:55 +0200

    initial commit
    ^E
    
    initial commit#

---
+++ src/test/java/org/pipproject/pip_project/PipProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pipproject.pip_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
