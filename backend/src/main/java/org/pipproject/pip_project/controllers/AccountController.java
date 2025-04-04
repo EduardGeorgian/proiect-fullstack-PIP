28a0e6b2a08dd373af19c9c948eae86f9cf70f9f
Eduard <edi.eduard1555@gmail.com>
2025-04-04 09:31:25 +0300

    implemented get method for accounts found by user email, meant for transactions from another user

--- backend/src/main/java/org/pipproject/pip_project/controllers/AccountController.java
+++ backend/src/main/java/org/pipproject/pip_project/controllers/AccountController.java
@@ -1,14 +1,21 @@
 package org.pipproject.pip_project.controllers;
 
 import org.pipproject.pip_project.business.AccountService;
+import org.pipproject.pip_project.business.UserService;
 import org.pipproject.pip_project.dto.AccountCreateDTO;
+import org.pipproject.pip_project.dto.AccountSendTransactionDTO;
+import org.pipproject.pip_project.dto.UserWithAccountsDTO;
 import org.pipproject.pip_project.model.Account;
+import org.pipproject.pip_project.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/account")
@@ -46,4 +53,16 @@ public class AccountController {
         }
     }
 
+    @GetMapping("/{userEmail}")
+    public ResponseEntity<?> getAccountsForTransactions(@PathVariable String userEmail){
+        try {
+            List<AccountSendTransactionDTO> response= accountService.getAccountSendTransactionsByUser(userEmail);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }
+        catch (Exception e) {
+            Map<String,String> response = new HashMap<>();
+            response.put("error",e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
 }
--- backend/src/main/java/org/pipproject/pip_project/repositories/AccountRepository.java
+++ backend/src/main/java/org/pipproject/pip_project/repositories/AccountRepository.java
@@ -1,8 +1,10 @@
 package org.pipproject.pip_project.repositories;
 
+import org.pipproject.pip_project.dto.AccountSendTransactionDTO;
 import org.pipproject.pip_project.model.Account;
 import org.pipproject.pip_project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,7 @@ import java.util.Optional;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
    List<Account> findByUser(Optional<User> user);
+   @Query("SELECT new org.pipproject.pip_project.dto.AccountSendTransactionDTO(a.id, a.currency) " +
+           "FROM Account a WHERE a.user = :user")
+   List<AccountSendTransactionDTO> findAccountSendTransactionDTOByUser(Optional<User> user);
 }
